a90b6f07bc894606f44de948d0d63b2678ed574b
Brave Leuterio <you@example.com>
2020-06-16 23:19:15 +0800

    fixed: overflow of bottomsheet and mainbody

--- lib/components/taskbottomsheet.dart
+++ lib/components/taskbottomsheet.dart
@@ -16,55 +16,62 @@ class _TaskBottomSheetState extends State<TaskBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 55),
-      color: Colors.white,
-      height: 270,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Add Task',
-            style: TextStyle(
-              color: Colors.blueAccent,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          TextField(
-            controller: textEditingController,
-            decoration: InputDecoration(
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.blueAccent),
-              ),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.blueGrey),
-              ),
-            ),
-            onChanged: (value) {
-              taskTitle = value;
-            },
-          ),
-          SizedBox(height: 25),
-          ButtonTheme(
-            minWidth: double.infinity,
-            child: FlatButton(
-              color: Colors.blueAccent,
-              padding: EdgeInsets.all(20),
-              onPressed: () {
-                textEditingController.clear();
-                widget.callback(taskTitle);
-              },
-              child: Text(
-                'Add',
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SingleChildScrollView(
+        child: Container(
+          height: 230,
+          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 55),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Add Task',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.blueAccent,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          )
-        ],
+              TextField(
+                controller: textEditingController,
+                autofocus: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueGrey),
+                  ),
+                ),
+                onChanged: (value) {
+                  taskTitle = value;
+                },
+              ),
+              SizedBox(height: 25),
+              ButtonTheme(
+                minWidth: double.infinity,
+                child: FlatButton(
+                  color: Colors.blueAccent,
+                  padding: EdgeInsets.all(20),
+                  onPressed: () {
+                    textEditingController.clear();
+                    widget.callback(taskTitle);
+                  },
+                  child: Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -20,6 +20,7 @@ class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.blueAccent,
         floatingActionButton: FloatingActionButton(
           onPressed: () {
